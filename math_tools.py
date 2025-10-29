3d2852a7f64a2ea345fb0cfb3959e3265d8b052b
Yasser AL-Qulayit <yasirqulayti@gmail.com>
2025-10-30 01:11:06 +0300

    Day 3: Simple math functions

---
+++ math_tools.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mult(a,b):
+    return a * b
+def div(a,b):
+    return a / b
+
+print(add(4,5))
+print(sub(10,3))
+print(mult(5,5))
+print(div(100,10))
